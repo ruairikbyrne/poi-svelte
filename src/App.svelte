3f4cc1e142bc6c9b2b3aceb57a3b5f228df5aa5a
Ruairi Byrne <ruairikbyrne@hotmail.com>
2021-05-22 13:44:26 +0100

    update server address

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
   import Signup from "./pages/Signup.svelte";
   import Router from "svelte-spa-router";
 
-  setContext("POIService", new POIService("http://localhost:4000"))
+  setContext("POIService", new POIService("https://tranquil-hamlet-82612.herokuapp.com/"))
 
   let routes = {
     "/": Main,
